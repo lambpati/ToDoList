dda6f3d7047b91022da25bb3277c290fecba9674
Patience Lamb <59948451+lambpati@users.noreply.github.com>
2020-10-20 19:00:31 -0600

    Created GUI

---
+++ src/main/java/ToDoListGUI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class ToDoListGUI {
+    private JButton deletetask;
+    private JButton newtask;
+    private JCheckBox active;
+    private JCheckBox archived;
+
+
+}
