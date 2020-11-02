6ec51e6eab4c83521c722ad348545c7b156b895e
lambpati <lambpati@isu.edu>
2020-11-01 22:28:44 -0700

    Creating simple tests for Item and ToDoList.

---
+++ src/test/java/ItemTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+class ItemTest {
+
+    @Test
+    void subscribe() {
+    }
+
+    @Test
+    void unsubscribe() {
+    }
+
+    @Test
+    void notifySubscribers() {
+    }
+}
---
+++ src/test/java/ToDoListTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+class ToDoListTest {
+
+    @Test
+    void addItem() {
+    }
+
+    @Test
+    void removeItem() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void getItem() {
+    }
+}
