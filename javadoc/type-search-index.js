743a7ce84eba12483b7de30384047b0d5b7cb00c
Patience Lamb <59948451+lambpati@users.noreply.github.com>
2020-11-03 01:03:19 -0700

    Committing JavaDocs
    Since Javadocs creates in /build, needed to create a seperate javadocs folder to ensure files can be found without having to be generated.

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"ButtonHandler"},{"p":"<Unnamed>","l":"Observer"},{"p":"<Unnamed>","l":"Publisher"},{"p":"<Unnamed>","l":"readFromFile"},{"p":"<Unnamed>","l":"saveToFile"},{"p":"<Unnamed>","l":"ToDoItem"},{"p":"<Unnamed>","l":"ToDoList"},{"p":"<Unnamed>","l":"ToDoListGUI"}]
