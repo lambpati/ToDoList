dc644174fa8d3f16ee5ba72c5b954ee5bcffdec0
lambpati <lambpati@isu.edu>
2020-11-15 19:43:00 -0700

    Writing more of GUI
    Creating GUI, having issues with main() not wanting to work with no errors to indicate problem.

---
+++ src/main/java/ToDoListUI.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+public class ToDoListUI {
+    private JButton newButton;
+    private JButton deleteButton;
+    private JTable toDoTable;
+    private JPanel jPanel;
+    private ListManager listManager = new ListManager();
+    private DefaultTableModel model = new DefaultTableModel();
+    String[] columnNames = {"Task"};
+
+    public void loadData(){
+        listManager.initialPopulate();
+        toDoTable.setModel(model);
+        model.setColumnIdentifiers(columnNames);
+        if(!listManager.getManagedList().getItems().isEmpty()) {
+            for (String s : listManager.getManagedList().getItems()) {
+                for (int i = 0; i < listManager.getManagedList().getItems().size(); i++) {
+                    Object[] o = new Object[listManager.getManagedList().getItems().size()];
+                    o[i] = s;
+                    model.addRow(o);
+                }
+            }
+        }
+        else {
+            Object[] o = new Object[0];
+            o[0] = "Add new tasks!";
+            model.addRow(o);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("ToDoListUI");
+        frame.setContentPane(new ToDoListUI().jPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+//        ToDoListUI toDoListUI = new ToDoListUI();
+//        toDoListUI.loadData();
+
+    }
+}
